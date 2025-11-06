347bf0dc069171608f38d73f8bb1a4a6b66e1e87
yaritza lopez <160291834+rouuhehe@users.noreply.github.com>
2025-11-05 20:57:04 -0500

    Add files via upload

--- subir_archivo.py
+++ subir_archivo.py
@@ -1,33 +1,40 @@
 import boto3
-import base64
+#https://i.pinimg.com/736x/ff/dd/86/ffdd8691730d5ca5688450e93cd8d60e.jpg
+import requests
+import json
+
+s3 = boto3.client('s3')
+BUCKET = "baldecito-tarea"
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
-
-    # Parámetros desde el evento
-    bucket_name = "baldecito-tarea"
-    folder_name =  "nuevacarpeta/"
-    file_name = "gatitos.png"
-    base64_str = event.get("base64_str")
-
-    if not base64_str:
-        return {
-            'statusCode': 400,
-            'body': "Falta el parámetro 'base64_str' con la imagen codificada."
-        }
-
     try:
-        s3_file_name = f"{folder_name}{file_name}"
+        body = json.loads(event["body"])
+        image_url = body["image_url"]
+        file_name = body["file_name"]
+        folder_name = body["folder_name"]
+        key = f"{folder_name}{file_name}"
 
-        s3.Object(bucket_name, s3_file_name).put(Body=base64.b64decode(base64_str))
+        # Descargar la imagen desde el enlace
+        response = requests.get(image_url)
+        if response.status_code != 200:
+            return {
+                "statusCode": 400,
+                "body": json.dumps({"error": "No se pudo descargar la imagen"})
+            }
+
+        # Subir a S3
+        s3.put_object(Bucket=BUCKET, Key=key, Body=response.content)
 
         return {
-            'statusCode': 200,
-            'body': f"Imagen subida exitosamente en s3://{bucket_name}/{s3_file_name}"
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "Imagen subida correctamente",
+                "s3_path": f"s3://{BUCKET}/{key}"
+            })
         }
 
     except Exception as e:
         return {
-            'statusCode': 500,
-            'body': f"Error subiendo imagen: {str(e)}"
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
         }
