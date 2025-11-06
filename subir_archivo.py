366076506f909e3163dd3836b755c1a6e50b09d5
yaritza lopez <160291834+rouuhehe@users.noreply.github.com>
2025-11-05 21:00:39 -0500

    Add files via upload

--- subir_archivo.py
+++ subir_archivo.py
@@ -1,6 +1,6 @@
 import boto3
 #https://i.pinimg.com/736x/ff/dd/86/ffdd8691730d5ca5688450e93cd8d60e.jpg
-import requests
+import urllib.request
 import json
 
 s3 = boto3.client('s3')
@@ -14,16 +14,10 @@ def lambda_handler(event, context):
         folder_name = body["folder_name"]
         key = f"{folder_name}{file_name}"
 
-        # Descargar la imagen desde el enlace
-        response = requests.get(image_url)
-        if response.status_code != 200:
-            return {
-                "statusCode": 400,
-                "body": json.dumps({"error": "No se pudo descargar la imagen"})
-            }
+        with urllib.request.urlopen(image_url) as response:
+            image_data = response.read()
 
-        # Subir a S3
-        s3.put_object(Bucket=BUCKET, Key=key, Body=response.content)
+        s3.put_object(Bucket=BUCKET, Key=key, Body=image_data)
 
         return {
             "statusCode": 200,
@@ -32,7 +26,7 @@ def lambda_handler(event, context):
                 "s3_path": f"s3://{BUCKET}/{key}"
             })
         }
-
+    
     except Exception as e:
         return {
             "statusCode": 500,
